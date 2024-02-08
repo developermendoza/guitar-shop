a52271b82abe5ef8a570cb776868ff0922d48274
Jose Mendoza <developermendoza@gmail.com>
2024-02-07 20:05:39 -0800

    remove prop from Carousel that is giving error

--- src/app/components/featured-products.tsx
+++ src/app/components/featured-products.tsx
@@ -54,7 +54,6 @@ export default function FeaturedProducts() {
         arrows
         autoPlaySpeed={3000}
         centerMode={false}
-        // containerClass="container"
         dotListClass=""
         draggable
         focusOnSelect={false}
@@ -66,36 +65,6 @@ export default function FeaturedProducts() {
         renderArrowsWhenDisabled={false}
         renderButtonGroupOutside={false}
         renderDotsOutside={false}
-        afterChange={(previousSlide, { currentSlide, onMove }) => {
-          console.log("test", currentSlide);
-        }}
-        // focusOnSelect={currentItem}
-        // responsive={{
-        //   desktop: {
-        //     breakpoint: {
-        //       max: 3000,
-        //       min: 1024,
-        //     },
-        //     items: 3,
-        //     partialVisibilityGutter: 40,
-        //   },
-        //   mobile: {
-        //     breakpoint: {
-        //       max: 464,
-        //       min: 0,
-        //     },
-        //     items: 1,
-        //     partialVisibilityGutter: 30,
-        //   },
-        //   tablet: {
-        //     breakpoint: {
-        //       max: 1024,
-        //       min: 464,
-        //     },
-        //     items: 2,
-        //     partialVisibilityGutter: 30,
-        //   },
-        // }}
         rewind={true}
         rewindWithAnimation={false}
         rtl={false}
